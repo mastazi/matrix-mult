04cc4aa23fe59b7239fd3aeb187fea081f987977
Marco Stazi <marco.stazi@gmail.com>
2021-03-02 20:32:01 +1100

    added all tests

--- tests/Unit/MatrixTest.php
+++ tests/Unit/MatrixTest.php
@@ -12,6 +12,7 @@ class MatrixTest extends TestCase
     protected $notRectangular;
     protected $not2D;
     protected $notMultipliable;
+    protected $expectedResult;
 
     /**
      * Setting up all the matrices needed for testing
@@ -26,6 +27,7 @@ class MatrixTest extends TestCase
         $this->not2D = array(1,2,3);
         // The row length of the following array is != number of rows of matrixB
         $this->notMultipliable = array(array(1, 2, 3, 4), array(5, 6, 7, 8));
+        $this->expectedResult = array(array(58, 64), array(139, 154));
     }
     /**
      * Testing that the matrix should be 2D
@@ -39,5 +41,19 @@ class MatrixTest extends TestCase
         $this->assertTrue($matrix::isMatrix($this->matrixA));
         $this->assertFalse($matrix::isMatrix($this->not2D));
         $this->assertFalse($matrix::isMatrix($this->notRectangular));
+        $this->assertFalse($matrix::canMultiply($this->notMultipliable, $this->matrixB));
+    }
+    /**
+     * test the matrix multiplication,
+     * using the example at
+     * https://www.mathsisfun.com/algebra/matrix-multiplying.html
+     * (hence known to be correct)
+     *
+     * @return void
+     */
+    public function testMultiply()
+    {
+        $matrix = new MatrixService();
+        $this->assertEquals($matrix::multiply($this->matrixA, $this->matrixB), $this->expectedResult);
     }
 }
